a2b7eb3ece001ca349f4bef700a78e1ef226fdc1
Camilo1090149643 <cperezd6@miumg.edu.gt>
2022-08-16 22:14:49 -0600

    vista-disenio

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Pruebas')
+
+@section('content_header')
+    <h1>Bienvenido a la Vista adminlte</h1>
+@stop
+
+@section('content')
+
+
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
